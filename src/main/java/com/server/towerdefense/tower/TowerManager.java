9e05f7f92316c59c58be87035c3e77c76a29ceef
Nagiev217 <doich3609@gmail.com>
2026-05-14 21:57:11 +0400

    Update TowerManager.java

--- src/main/java/com/server/towerdefense/tower/TowerManager.java
+++ src/main/java/com/server/towerdefense/tower/TowerManager.java
@@ -5,6 +5,8 @@ import com.server.towerdefense.config.ConfigManager;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -55,6 +57,7 @@ public class TowerManager {
 
     public Tower placeTower(Arena arena, Player owner, Location baseLocation, TowerType type) {
         String root = "towers." + type.getConfigKey() + ".";
+        double maxHealth = configManager.getConfig().getDouble(root + "max-health", 100.0);
         double damage = configManager.getConfig().getDouble(root + "damage");
         double range = configManager.getConfig().getDouble(root + "range");
         int attackSpeedTicks = configManager.getConfig().getInt(root + "attack-speed-ticks");
@@ -62,7 +65,8 @@ public class TowerManager {
         Location towerLocation = baseLocation.getBlock().getLocation();
         buildVisual(towerLocation, type);
 
-        Tower tower = new Tower(UUID.randomUUID(), owner.getUniqueId(), towerLocation, type, damage, range, attackSpeedTicks);
+        ArmorStand healthDisplay = createHealthDisplay(towerLocation);
+        Tower tower = new Tower(UUID.randomUUID(), owner.getUniqueId(), towerLocation, type, maxHealth, damage, range, attackSpeedTicks, healthDisplay);
         arena.getActiveTowers().add(tower);
         return tower;
     }
@@ -92,9 +96,21 @@ public class TowerManager {
         Location location = tower.getLocation();
         location.getBlock().setType(org.bukkit.Material.AIR);
         location.clone().add(0, 1, 0).getBlock().setType(org.bukkit.Material.AIR);
+        tower.removeHealthDisplay();
     }
 
     public NamespacedKey getTowerItemKey() {
         return towerItemKey;
+    }
+    
+    private ArmorStand createHealthDisplay(Location towerLocation) {
+        Location displayLocation = towerLocation.clone().add(0.5, 2.25, 0.5);
+        ArmorStand armorStand = (ArmorStand) towerLocation.getWorld().spawnEntity(displayLocation, EntityType.ARMOR_STAND);
+        armorStand.setInvisible(true);
+        armorStand.setMarker(true);
+        armorStand.setGravity(false);
+        armorStand.setInvulnerable(true);
+        armorStand.setCustomNameVisible(true);
+        return armorStand;
     }
 }
