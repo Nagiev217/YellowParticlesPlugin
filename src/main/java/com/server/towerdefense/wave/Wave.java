add3f747d94666fcaf609457dd503fd47d2604d3
Nagiev217 <doich3609@gmail.com>
2026-05-14 21:14:33 +0400

    Create Wave.java

---
+++ src/main/java/com/server/towerdefense/wave/Wave.java
@@ -0,0 +1,55 @@
+package com.server.towerdefense.wave;
+
+import java.util.List;
+
+public class Wave {
+    private final int number;
+    private final List<WaveMobEntry> entries;
+    private final int startDelayTicks;
+    private boolean active;
+    private int spawnedMobs;
+    private int totalMobs;
+
+    public Wave(int number, List<WaveMobEntry> entries, int startDelayTicks) {
+        this.number = number;
+        this.entries = entries;
+        this.startDelayTicks = startDelayTicks;
+        this.totalMobs = entries.stream().mapToInt(WaveMobEntry::getAmount).sum();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public List<WaveMobEntry> getEntries() {
+        return entries;
+    }
+
+    public int getStartDelayTicks() {
+        return startDelayTicks;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getSpawnedMobs() {
+        return spawnedMobs;
+    }
+
+    public void incrementSpawnedMobs() {
+        spawnedMobs++;
+    }
+
+    public int getTotalMobs() {
+        return totalMobs;
+    }
+
+    public boolean hasSpawnedAll() {
+        return spawnedMobs >= totalMobs;
+    }
+}
